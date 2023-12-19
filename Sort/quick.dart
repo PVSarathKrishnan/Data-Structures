a114823044cadc0c7313e4c357c8f9e209c9335b
Alto b <altob282@gmail.com>
2023-12-19 23:28:19 +0530

    19/12

--- Sort/quick.dart
+++ Sort/quick.dart
@@ -1,5 +1,5 @@
 
-class quicksort{
+class quickSort{
 
   List<int> quick(List<int>arr){
     sort(arr, 0, arr.length-1);
@@ -11,11 +11,12 @@ class quicksort{
     if(str>=end){
       return;
     }
+     
     int pivot = str;
     int left = str+1;
     int right = end;
 
-    while(left <= right){
+    while(left<=right){
 
       if(arr[left] > arr[pivot] && arr[right] < arr[pivot]){
         swap(arr, left, right);
@@ -27,26 +28,28 @@ class quicksort{
         left++;
       }
 
-      if(arr[right] >= arr[pivot]){
+      if(arr[right]>=arr[pivot]){
         right--;
       }
-   }
-  swap(arr, right, pivot);
-  sort(arr, str, right-1);
-  sort(arr, right+1, end);
+    }
+    swap(arr,right,pivot);//swap pivot and right
+    sort(arr, str, right-1);//sort the left partition
+    sort(arr, right+1, end);//sort the right partition 
+    
   }
-
-  void swap(List<int>arr,int i ,int j){
+  
+  void swap(List<int> arr, int i, int j ){
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
-  }
-
+    }
+    
 }
-
 void main(){
-List<int> arr = [1,3,4,2,5,7,2];
 
-quicksort q =quicksort();
-print(q.quick(arr));
+  List<int> arr = [1,4,3,6,4,3,9];
+
+  quickSort q =quickSort();
+  print(q.quick(arr));
+
 }
