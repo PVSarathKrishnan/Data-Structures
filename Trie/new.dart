345586add7eb5654f668b05f27f0512a9ecdaead
Sarath Krishnan P V <142596951+PVSarathKrishnan@users.noreply.github.com>
2024-02-01 15:09:43 +0530

    Updated and added Suffix and prefix code
    Update on existing code

--- Trie/new.dart
+++ Trie/new.dart
@@ -1,7 +1,6 @@
 //suffix - Trie
 
 import 'dart:collection';
-
 class TrieNode{
   HashMap<String, TrieNode> children = HashMap();
   bool end = false;
@@ -35,10 +34,40 @@ for(int i = word.length-1;i>=0;i--){
     return true;
   }
 }
-void main() {
-  Trie newTrie = Trie();
-  newTrie.insertWord("farhan");
-  
-  print(newTrie.containsSuffix("far")); // Output: false
-  print(newTrie.containsSuffix("han")); // Output: true
+
+//Prefix Trie -  Just changing loop structure 
+
+class TrieNode {
+  HashMap<String, TrieNode> children = HashMap();
+  bool end = false;
+}
+
+class Trie {
+  TrieNode root = TrieNode();
+
+  void insertWord(String word) {
+    TrieNode temp = root;
+
+    for (int i = 0; i < word.length; i++) {
+      if (!temp.children.containsKey(word[i])) {
+        TrieNode newNode = TrieNode();
+        temp.children[word[i]] = newNode;
+      }
+      temp = temp.children[word[i]]!;
+    }
+    temp.end = true; // Mark the end of the inserted word.
+  }
+
+  bool containsPrefix(String prefix) {
+    TrieNode temp = root;
+
+    for (int i = 0; i < prefix.length; i++) {
+      if (temp.children[prefix[i]] == null ||
+          !temp.children.containsKey(prefix[i])) {
+        return false;
+      }
+      temp = temp.children[prefix[i]]!;
+    }
+    return true; // Return true for any prefix of inserted words.
+  }
 }
